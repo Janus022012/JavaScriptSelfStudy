b5d9351c2399d58b486353788bfd3498ac4d42b4
janus022012 <janus022012@gmail.com>
2020-07-06 22:00:01 +0900

    Advanced React and Redux after lesson20

--- ReactStudy/testing/src/components/__tests__/App.test.js
+++ ReactStudy/testing/src/components/__tests__/App.test.js
@@ -1,3 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
+
+it('it shows a comment box', () => {
+    const div = document.createElement('div');
+
+    ReactDOM.render(<App/>, div);
+
+    // ブラウザがJSDOMによってシミュレートされ、その後Rreactコンポーネントがレンダリングされる。
+    // レンダリング後のHTMLによってテストを行う。
+    expect(div.innerHTML).toContainElement('CommentBox');
+
+    // unmountComponentAtNodeはComponenntのクリーンアップである。
+    ReactDOM.unmountComponentAtNode(div);
+});
