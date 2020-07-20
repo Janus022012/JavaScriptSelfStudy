1d1bd3313463fbd7d8c0a13f4ef029b6c78e9b9c
janus022012 <janus022012@gmail.com>
2020-07-20 21:32:22 +0900

    20200720

--- ReactStudy/takasho_example/src/components/App.js
+++ ReactStudy/takasho_example/src/components/App.js
@@ -2,16 +2,9 @@ import React, {useEffect, useState} from 'react';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {ThemeProvider} from "@material-ui/styles";
 import {Header} from "./ui/Header";
-import {ExampleDraggable} from "./ui/moveable_objects/example_draggable"
 import theme from './theme/Theme'
 
 export const App = () => {
-    const [loadingState, setloadingState] = useState(true);
-
-    useEffect(() => {
-
-    });
-
     return (
           <ThemeProvider theme={theme}>
             <BrowserRouter>
@@ -19,7 +12,6 @@ export const App = () => {
               <div>Takasho</div>
               <Switch>
               </Switch>
-            <ExampleDraggable/>
             </BrowserRouter>
           </ThemeProvider>
     );
